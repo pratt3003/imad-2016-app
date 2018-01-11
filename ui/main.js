bd2a15942f558ff812c2c5b03d4283c38f2d7009
Prashant <pratt.gennext@gmail.com>
2018-01-12 00:30:43 +0530

    client javascript

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,14 @@
 console.log('Loaded!');
+//move image on click
+
+var image= document.getElementById('madi');
+var marginLeft=0;
+function moveRight(){
+	marginLeft= marginLeft + 5;
+	image.style.marginLeft = marginLeft+ 'px';
+}
+console.log("fine");
+
+image.onclick = function(){
+	var interval= setInterval(moveRight,50);
+};
